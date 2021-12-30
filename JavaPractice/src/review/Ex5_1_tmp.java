78a4de02ec7bcd164a8c451a8b1b77184a3315e8
kimkim89 <kimkim909089@gmail.com>
2021-12-30 23:52:15 +0900

    배열 공부 완료

--- JavaPractice/src/review/Ex5_1_tmp.java
+++ JavaPractice/src/review/Ex5_1_tmp.java
@@ -24,18 +24,52 @@ public class Ex5_1_tmp {
 //			System.out.println("arr[" + i + "]= " + arr[i]);
 //		}
 		
-		int[] iArr = {100, 95, 80, 70, 60};
+//		int[] iArr = {100, 95, 80, 70, 60};
+//		
+//		for(int i=0; i<iArr.length; i++) {
+//			System.out.println(iArr[i]);
+//		}
+//		
+//		//배열을 문자열로 보여줌 
+//		System.out.println(Arrays.toString(iArr)); //결과: [100, 95, 80, 70, 60]
+//		
+//		//character형일 경우에만 println()함수 사용하면 문자들이 나옴
+//		char[] chArr = {'a', 'b', 'c', 'd' };
+//		System.out.println(chArr); //결과: abcd
 		
-		for(int i=0; i<iArr.length; i++) {
-			System.out.println(iArr[i]);
-		}
 		
-		//배열을 문자열로 보여줌 
-		System.out.println(Arrays.toString(iArr)); //결과: [100, 95, 80, 70, 60]
+//		String str = "ABCDE";
+//		char ch = str.charAt(3);
+//		String str2 = str.substring(1, 4);
+//		String str3 = str.substring(1);
+//		System.out.println(str3);
 		
-		//character형일 경우에만 println()함수 사용하면 문자들이 나옴
-		char[] chArr = {'a', 'b', 'c', 'd' };
-		System.out.println(chArr); //결과: abcd
+		
+//		int[] arr = {0, 1, 2, 3, 4};
+		int[] arr = {3, 4, 1, 0, 2};
+		
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr); // 배열 arr을 오름차순으로 정렬
+		System.out.println(Arrays.toString(arr));
+		
+		int[][] arr2D = { {11, 12}, {21, 22}};
+		
+//		System.out.println(Arrays.toString(arr));
+//		System.out.println(Arrays.deepToString(arr2D));
+		
+		
+		String[][] str2D = {{"aaa", "bbb"}, {"AAA", "BBB"}};
+		String[][] str2D2 = {{"aaa", "bbb"}, {"AAA", "BBB"}};
+		
+		System.out.println(str2D == str2D2); // 참조변수 값 비교
+		System.out.println(Arrays.deepEquals(str2D, str2D2));
+		
+		int[] arr2 = Arrays.copyOf(arr,  arr.length);
+		System.out.println(Arrays.toString(arr2));
+		
+		int[] arr3 = Arrays.copyOfRange(arr, 2,4);
+		System.out.println(Arrays.toString(arr3));
+		
 		
 		
 	}
